88e5656b294e716d7cb5d33467cdeba2d4488c89
Amy309 <125324963+Amy309@users.noreply.github.com>
2023-10-28 16:13:53 -0600

    Relleno

---
+++ laboratorio3/Student.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace laboratorio3
+{
+    public class Student
+    {
+        public int id { get; set; }
+        public string? nombre { get; set; }
+        public int unidadesValorativas { get; set; }
+        public string? ciclo { get; set; }
+        public int inscritos { get; set; }
+    }
+}
